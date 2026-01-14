e6c0cc8cfcb8efc8fa307caf77d0ae9ee7af7db8
JSB27lp <1conscienceofficiel@gmail.com>
2026-01-14 01:02:18 +0100

    Update CACHE_VERSION in service worker for PWA

--- dxm.service.worker.js
+++ dxm.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768348664|44228910';
+const CACHE_VERSION = '1768348928|308471908';
 /** @type {string} */
 const CACHE_PREFIX = 'deus x medicine-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
