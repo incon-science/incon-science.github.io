28bda6df1af96beec39160db272deae5d4d075f6
JSB27lp <1conscienceofficiel@gmail.com>
2026-01-15 21:33:18 +0100

    fd

--- dxm.service.worker.js
+++ dxm.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768498179|1240355738';
+const CACHE_VERSION = '1768509188|698511743';
 /** @type {string} */
-const CACHE_PREFIX = 'Deus X Medicine-sw-cache-';
+const CACHE_PREFIX = 'Deus x medicine-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'dxm.offline.html';
