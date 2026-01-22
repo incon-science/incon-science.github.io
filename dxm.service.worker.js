5d0b698ec70ba936767c168d63c31338dc767b77
JSB27lp <1conscienceofficiel@gmail.com>
2026-01-22 13:01:40 +0100

    fd

--- dxm.service.worker.js
+++ dxm.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1769082613|8965040155';
+const CACHE_VERSION = '1769083296|9647848272';
 /** @type {string} */
 const CACHE_PREFIX = 'Deus x medicine-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
