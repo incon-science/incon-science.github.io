ae2c37cde268f34c1e700e4083d2641201caec87
incon-science <1conscienceofficiel@gmail.com>
2026-02-13 10:52:51 +0100

    gf

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1770974855|8194062844';
+const CACHE_VERSION = '1770976364|9702979139';
 /** @type {string} */
 const CACHE_PREFIX = 'find_me-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
