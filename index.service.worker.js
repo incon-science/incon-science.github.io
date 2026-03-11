fffa91935fd7bec19c6cb2f9899a4e08c8c55d71
incon-science <1conscienceofficiel@gmail.com>
2026-03-11 23:04:58 +0100

    fsd

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773154621|16103965570';
+const CACHE_VERSION = '1773266674|44703562953';
 /** @type {string} */
-const CACHE_PREFIX = 'Voice of the Voi-sw-cache-';
+const CACHE_PREFIX = 'Lettre d'amour à-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
