3c23599b84fc72b6a5df0797525228b728547743
incon-science <1conscienceofficiel@gmail.com>
2026-03-16 11:19:08 +0100

    fd

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773652677|1714065683';
+const CACHE_VERSION = '1773656340|5377578095';
 /** @type {string} */
 const CACHE_PREFIX = 'Voice of the Voi-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
