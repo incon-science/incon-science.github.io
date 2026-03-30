deffa40b6464f7815221da679351d920069f3455
incon-science <1conscienceofficiel@gmail.com>
2026-03-31 01:23:52 +0200

    vdsf

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1774906283|1795060920';
+const CACHE_VERSION = '1774913017|6108466555';
 /** @type {string} */
 const CACHE_PREFIX = 'only the Void-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
