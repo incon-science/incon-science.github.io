6b65dbb8fb2cb77543f639b89459aac37e385ba5
incon-science <1conscienceofficiel@gmail.com>
2026-03-30 12:38:20 +0200

    fd

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1774834745|34002586784';
+const CACHE_VERSION = '1774867079|5538829089';
 /** @type {string} */
 const CACHE_PREFIX = 'Antichambre-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
